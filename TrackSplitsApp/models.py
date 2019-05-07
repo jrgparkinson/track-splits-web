fbf74134c10c35d1ea942e4eacd34a9d8deba117
Jamie Parkinson <jamie.parkinson@gmail.com>
2019-05-07 13:10:56 +0100

    Update models.py

--- TrackSplitsApp/models.py
+++ TrackSplitsApp/models.py
@@ -4,16 +4,16 @@ import datetime
 
 
 class Activity(models.Model):
-    activity_id = models.IntegerField()
+    activity_id = models.BigIntegerField()
     name = models.CharField(max_length=500)
     moving_time = models.DurationField()
     date = models.DateField(default=datetime.date.today)
-    athlete_id = models.IntegerField(default=-1)
+    athlete_id = models.BigIntegerField(default=-1)
     workout_type = models.IntegerField(default=-1)
 
     
 class Athlete(models.Model):
-    athlete_id = models.IntegerField()
+    athlete_id = models.BigIntegerField()
     first_name = models.CharField(max_length=500)
     
     
